43967922555c51b800893c4e9b826cdf236fae87
Nikita Rusetskii <rusetscky@outlook.com>
2022-06-16 19:23:40 +0800

    add formatting to FastTextDetector

--- src/main/java/ru/rusetskii/lingua/detector/FastTextDetector.java
+++ src/main/java/ru/rusetskii/lingua/detector/FastTextDetector.java
@@ -3,9 +3,11 @@ package ru.rusetskii.lingua.detector;
 import com.github.jfasttext.JFastText;
 import ru.rusetskii.lingua.model.DetectionResult;
 
+import java.util.List;
+
 public class FastTextDetector extends AbstractDetector {
 
-    private JFastText jft;
+    private final JFastText jft;
 
     public FastTextDetector() {
         jft = new JFastText();
@@ -14,7 +16,13 @@ public class FastTextDetector extends AbstractDetector {
 
     @Override
     public DetectionResult detect(String input) {
-        JFastText.ProbLabel probLabel = jft.predictProba(input);
-        return new DetectionResult(probLabel.label + " " + Math.exp(probLabel.logProb), 0);
+        long start = System.currentTimeMillis();
+        List<JFastText.ProbLabel> probLabel = jft.predictProba(input, 5);
+        long end = System.currentTimeMillis();
+        String res = "";
+        for (JFastText.ProbLabel label : probLabel) {
+            res += label.label + " : " + String.format("%.09f", Math.exp(label.logProb)) + ", ";
+        }
+        return new DetectionResult(res, end - start);
     }
 }
